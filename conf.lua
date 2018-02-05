46e14c0074fca1149b99f8a5b4fdeafbef455ab3
RafaelGiordanno <rafael_giordanno@hotmail.com>
2018-02-05 19:35:30 +0000

    Added bare bones of map collision detection

--- conf.lua
+++ conf.lua
@@ -1,5 +1,6 @@
 function love.conf(t)
     t.title = 'SheetEngine'
+    t.console = true
     t.window.width = 16 * 20
     t.window.height = 9 * 20
     t.modules.keyboard = true
--- main.lua
+++ main.lua
@@ -1,8 +1,22 @@
 local Terebi = require("terebi")
+local lume = require("lume")
 local screen = nil
 
 local GAME_WIDTH = 16 * 20
 local GAME_HEIGHT = 9 * 20
+
+local GRAVITY = 9.8
+local player = {
+    pos = { x = 120, y = 24 },
+    vel = { x = 0, y = 0 },
+    w = 16, 
+    h = 16
+}
+
+-- map is a linked list of points
+map = { next = map, x = 80, y = 140 }
+map = { next = map, x = 180, y = 140}
+map = { next = map, x = 196, y = 156}
 
 function love.load(arg)
     for k, v in ipairs(arg) do
@@ -19,11 +33,59 @@ function love.load(arg)
 end
 
 function love.update(dt)
+    player.vel.y = player.vel.y + GRAVITY * dt
 
+    -- checking collisions...
+    local node = map
+    local nextNode = map.next
+    while nextNode do
+        local intersect = {}
+        local bx = node.x
+        local by = node.y
+        local ex = nextNode.x
+        local ey = nextNode.y
+
+        local px1 = player.pos.x
+        local py1 = player.pos.y + player.vel.y
+        local px2 = player.pos.x
+        local py2 = player.pos.y + player.h + player.vel.y
+
+        -- checking vertical collision
+        -- to do: this is not working, fix it
+        if intersectLines(px1, py1, px2, py2, bx, by, ex, ey, intersect) then
+            player.vel.y = 0
+            print("colliding")
+        end
+        -- switching nodes
+        node = nextNode
+        nextNode = nextNode.next
+    end
+
+    player.vel.x = lume.clamp(player.vel.x, -4, 4)
+    player.vel.y = lume.clamp(player.vel.y, -4, 4)
+
+    -- updating player's position
+    player.pos.x = player.pos.x + player.vel.x
+    player.pos.y = player.pos.y + player.vel.y
+end
+
+function render()
+    love.graphics.clear(12, 23, 77, 255)
+    -- the player
+    love.graphics.line(player.pos.x, player.pos.y, player.pos.x, player.pos.y + player.h)
+
+    -- the map
+    local node = map
+    local nextNode = map.next
+    while nextNode do
+        love.graphics.line(node.x, node.y, nextNode.x, nextNode.y)
+        node = nextNode
+        nextNode = nextNode.next
+    end
 end
 
 function love.draw()
-
+    screen:draw(render)
 end
 
 function love.keypressed(key)
@@ -40,4 +102,19 @@ function love.keyreleased(key)
     if key == 'f11' then
         screen:toggleFullscreen()
     end
+end
+
+-- math stuff goes here
+function intersectLines (x1, y1, x2, y2, x3, y3, x4, y4, intersection)
+    d = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+    if (d == 0) then return false end
+
+    if (intersection ~= nil) then
+        ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / d;
+        intersection = {
+            x = x1 + (x2 - x1) * ua,
+            y = y1 + (y2 - y1) * ua
+        }
+    end
+    return true
 end
